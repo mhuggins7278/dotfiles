1a49df072b2342767c6ef89e3d03d521cca5a1ef
Mark Huggins <mhuggins@glgroup.com>
2022-10-20 13:22:27 -0400

    fix up auto commands

--- config/lvim/lua/user/autocommands.lua
+++ config/lvim/lua/user/autocommands.lua
@@ -1,25 +1,40 @@
-vim.api.nvim_create_autocmd({ "BufNew", "BufRead" }, {
-	pattern = { "*.astro" },
-	command = "setf astro",
-})
+local create_autocmd = vim.api.nvim_create_autocmd
 
--- Thank you to ThePrimeagen
--- make timeout on yank faster
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-	pattern = { "*" },
-	callback = function()
-		vim.highlight.on_yank({
-			higroup = "IncSearch",
-			timeout = 40,
-		})
-	end,
-})
+local commands = {
+	{
+		{ "BufNew", "BufRead" },
+		{
+			pattern = { "*.astro" },
+			command = "setf astro",
+		},
+	},
+	{
+		{ "TextYankPost" },
+		{
+			pattern = { "*" },
+			callback = function()
+				vim.highlight.on_yank({
+					higroup = "IncSearch",
+					timeout = 40,
+				})
+			end,
+		},
+	},
+	{
+		{ "BufWritePost" },
+		{
+			pattern = { "plugins.lua" },
+			callback = function()
+				vim.cmd("PackerSync")
+				vim.cmd("PackerCompile")
+			end,
+		},
+	},
+}
 
-vim.api.nvim_create_autocmd({"BufWritePost"}, {
-pattern = {"plugins.lua"},
-callback = function()
-    vim.cmd("PackerSync")
-    vim.cmd("PackerCompile")
-  end
-})
+local autocmds = require "lvim.core.autocmds"
+
+
+
+autocmds.define_autocmds(commands)
 
