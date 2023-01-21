fbaa6edaa126adfa2eb182c2cf450515971c877a
Mark Huggins <mhuggins@glgroup.com>
2023-01-20 23:34:09 -0500

    clean up keymaps

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -109,8 +109,6 @@ return {
 
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
 			vim.keymap.set("n", "<leader>lca", vim.lsp.buf.code_action, opts)
 			vim.keymap.set("n", "<leader>lrr", vim.lsp.buf.references, opts)
 			vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, opts)
