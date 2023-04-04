4990a14287468f777d712f90ceac033ad581f006
Mark Huggins <mhuggins@glgroup.com>
2023-04-04 08:46:31 -0400

    :recycle: better nvim tree config

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -2,9 +2,14 @@ return {
   {
 	"nvim-tree/nvim-tree.lua",
 	config = {
+    sync_root_width_cwd = true,
 		view = {
 			float = {
 				enable = true,
+        open_win_config = {
+          width = 60,
+          border = "rounded",
+        },
 			},
 		},
 	},
