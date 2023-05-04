d1f5a0e41aaaae87a8d7e96888aeb0eb12b9d7b6
Mark Huggins <mhuggins@glgroup.com>
2023-05-04 12:10:15 -0400

    more cleanup of keys

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,35 +1,35 @@
 return {
-    {
-        'lewis6991/gitsigns.nvim',
-        event = 'BufRead',
-        config = function()
-            require('gitsigns').setup({
-              numhl = true,
-              linehl = false
-            })
-        end
-    },
-    {
-    function()
-      local wk = require('which-key')
-    wk.register({
-      ["g"] = {
-          name = "+git",
-        s = {':Gitsigns stage_hunk<CR>', 'Stage hunk' },
-        r = {':Gitsigns reset_hunk<CR>', 'Reset hunk' },
-        S = {':Gitsigns stage_buffer<CR>', 'Stage buffer' },
-        u = {':Gitsigns undo_stage_hunk<CR>', 'Undo stage hunk' },
-        R = {':Gitsigns reset_buffer<CR>', 'Reset buffer' },
-        P = {':Gitsigns preview_hunk<CR>', 'Preview hunk' },
-        B = {':Gitsigns blame_line<CR>', 'Blame line' },
-        b = {':Gitsigns toggle_current_line_blame<CR>', 'Toggle blame' },
-        d = {':Gitsigns diffthis<CR>', 'Diff this' },
-        D = {':Gitsigns diffthis<CR>', 'Diff this against HEAD' },
-        x = {':Gitsigns toggle_deleted<CR>', 'Toggle deleted' },
-        p = {':Gitsigns prev_hunk<CR>', 'Prev Hunk' },
-        n = {':Gitsigns next_hunk<CR>', 'Next Hunk' },
-    }, { prefix = "<leader>" })
-
-    end
-    }
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				numhl = true,
+				linehl = false,
+			})
+		end,
+	},
+	{
+		function()
+			local wk = require("which-key")
+			wk.register({
+				["g"] = {
+					name = "+git",
+					s = { ":Gitsigns stage_hunk<CR>", "Stage hunk" },
+					r = { ":Gitsigns reset_hunk<CR>", "Reset hunk" },
+					S = { ":Gitsigns stage_buffer<CR>", "Stage buffer" },
+					u = { ":Gitsigns undo_stage_hunk<CR>", "Undo stage hunk" },
+					R = { ":Gitsigns reset_buffer<CR>", "Reset buffer" },
+					P = { ":Gitsigns preview_hunk<CR>", "Preview hunk" },
+					B = { ":Gitsigns blame_line<CR>", "Blame line" },
+					b = { ":Gitsigns toggle_current_line_blame<CR>", "Toggle blame" },
+					d = { ":Gitsigns diffthis<CR>", "Diff this" },
+					D = { ":Gitsigns diffthis<CR>", "Diff this against HEAD" },
+					x = { ":Gitsigns toggle_deleted<CR>", "Toggle deleted" },
+					p = { ":Gitsigns prev_hunk<CR>", "Prev Hunk" },
+					n = { ":Gitsigns next_hunk<CR>", "Next Hunk" },
+				},
+				{ prefix = "<leader>" },
+			})
+		end,
+	},
 }
