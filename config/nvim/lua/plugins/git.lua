b64e73b22ccd7a852ed34811dd2fd9aca306f155
Mark Huggins <mhuggins@glgroup.com>
2023-05-04 11:48:02 -0400

    more which key changes

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -10,19 +10,26 @@ return {
         end
     },
     {
-        { vim.keymap.set({ 'n', 'v' }, '<leader>gs', ':Gitsigns stage_hunk<CR>', { desc = 'Stage hunk' }) },
-        { vim.keymap.set({ 'n', 'v' }, '<leader>gr', ':Gitsigns reset_hunk<CR>', { desc = 'Reset hunk' }) },
-        { vim.keymap.set('n', '<leader>gS', ':Gitsigns stage_buffer<CR>', { desc = 'Stage buffer' }) },
-        { vim.keymap.set('n', '<leader>gu', ':Gitsigns undo_stage_hunk<CR>', { desc = 'Undo stage hunk' }) },
-        { vim.keymap.set('n', '<leader>gR', ':Gitsigns reset_buffer<CR>', { desc = 'Reset buffer' }) },
-        { vim.keymap.set('n', '<leader>gP', ':Gitsigns preview_hunk<CR>', { desc = 'Preview hunk' }) },
-        { vim.keymap.set('n', '<leader>gB', ':Gitsigns blame_line<CR>', { desc = 'Blame line' }) },
-        { vim.keymap.set('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<CR>', { desc = 'Toggle blame' }) },
-        { vim.keymap.set('n', '<leader>gd', ':Gitsigns diffthis<CR>', { desc = 'Diff this' }) },
-        { vim.keymap.set('n', '<leader>gD', ':Gitsigns diffthis<CR>', { desc = 'Diff this against HEAD' }) },
-        { vim.keymap.set('n', '<leader>gx', ':Gitsigns toggle_deleted<CR>', { desc = 'Toggle deleted' }) },
-        { vim.keymap.set('n', '<leader>gp', ':Gitsigns prev_hunk<CR>', { desc = 'Prev Hunk' }) },
-        { vim.keymap.set('n', '<leader>gn', ':Gitsigns next_hunk<CR>', { desc = 'Next Hunk' }) },
+    function()
+      local wk = require('which-key')
+    wk.register({
+      ["g"] = {
+          name = "+git",
+        s = {':Gitsigns stage_hunk<CR>', 'Stage hunk' },
+        r = {':Gitsigns reset_hunk<CR>', 'Reset hunk' },
+        S = {':Gitsigns stage_buffer<CR>', 'Stage buffer' },
+        u = {':Gitsigns undo_stage_hunk<CR>', 'Undo stage hunk' },
+        R = {':Gitsigns reset_buffer<CR>', 'Reset buffer' },
+        P = {':Gitsigns preview_hunk<CR>', 'Preview hunk' },
+        B = {':Gitsigns blame_line<CR>', 'Blame line' },
+        b = {':Gitsigns toggle_current_line_blame<CR>', 'Toggle blame' },
+        d = {':Gitsigns diffthis<CR>', 'Diff this' },
+        D = {':Gitsigns diffthis<CR>', 'Diff this against HEAD' },
+        x = {':Gitsigns toggle_deleted<CR>', 'Toggle deleted' },
+        p = {':Gitsigns prev_hunk<CR>', 'Prev Hunk' },
+        n = {':Gitsigns next_hunk<CR>', 'Next Hunk' },
+    }, { prefix = "<leader>" })
 
+    end
     }
 }
