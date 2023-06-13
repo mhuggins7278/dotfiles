41ded15875ff61cd3d88e2dd3a6eb373433d9dc1
Mark Huggins <mhuggins@glgroup.com>
2023-06-13 08:30:01 -0400

    nvimgpt
    just adding nvim gpt and updating tmux plugins

---
+++ config/nvim/lua/plugins/nvimgpt.lua
@@ -0,0 +1,14 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd = "security find-generic-password -a $USER -s NVIM_CHAT_GPT_TOKEN -w",
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
