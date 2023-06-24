6a6ce58c99958bfd04a13744c1071bb6b31bb4e6
Mark Huggins <mhuggins@glgroup.com>
2023-06-24 12:58:12 -0400

    fix gpt config

--- config/nvim/lua/plugins/nvimgpt.lua
+++ config/nvim/lua/plugins/nvimgpt.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup({
-      api_key_cmd = "security find-generic-password -a $USER -s NVIM_CHAT_GPT_TOKEN -w",
+      api_key_cmd = "security find-generic-password -s NVIM_CHAT_GPT_TOKEN -w",
     })
   end,
   dependencies = {
