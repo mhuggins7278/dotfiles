362e61d0420e9ee06f1d97e855195c4d99bc9ccb
Mark Huggins <mhuggins@glgroup.com>
2023-09-07 08:48:08 -0400

    fix gitmux status not showing

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -4,14 +4,6 @@ return {
   opts = {},
   keys = {
     {
-      "s",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump()
-      end,
-      desc = "Flash",
-    },
-    {
       "S",
       mode = { "n", "o", "x" },
       function()
