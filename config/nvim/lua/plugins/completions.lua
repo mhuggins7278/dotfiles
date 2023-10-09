5f65a00c03ebdd541745ed8aa34a30a65d9a6b46
Mark Huggins <mhuggins@glgroup.com>
2023-10-08 23:01:06 -0400

    refactor(completions/lsp): no more lsp-zero

--- config/nvim/lua/plugins/completions.lua
+++ config/nvim/lua/plugins/completions.lua
@@ -1,5 +1,4 @@
 return {
-
   "hrsh7th/nvim-cmp",
   dependencies = {
     { "hrsh7th/cmp-vsnip" },
@@ -16,32 +15,47 @@ return {
     { "rafamadriz/friendly-snippets" }
   },
   config = function()
-    vim.opt.completeopt = { "menu", "menuone", "noselect" }
     vim.opt.shortmess:append("c")
+    local luasnip = require("luasnip")
 
     local cmp = require("cmp")
-    local cmp_format = require("lsp-zero").cmp_format()
     require('luasnip.loaders.from_vscode').lazy_load()
 
-
     cmp.setup({
-      preselect = cmp.PreselectMode.None,
+      preselect = 'none',
       completion = {
-        completeopt = 'menu,menuone,noinsert'
+        completeopt = 'menu,preview,menuone,noselect'
+      },
+      snippet = {       -- configure how nvim-cmp interacts with snippet engine
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
       },
       sources = {
-        { name = 'path' },
         { name = 'copilot' },
+        { name = 'path' },
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
         { name = 'buffer' },
         { name = 'luasnip' },
       },
-      formatting = cmp_format,
+      formatting = {
+        fields = { 'abbr', 'kind', 'menu' },
+        format = require('lspkind').cmp_format({
+          mode = 'symbol',       -- show only symbol annotations
+          maxwidth = 50,         -- prevent the popup from showing more than provided characters
+          symbol_map = { Copilot = "" },
+          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+        })
+      },
       mapping = cmp.mapping.preset.insert({
         -- scroll up and down the documentation window
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-n>"] = cmp.config.disable,
+        ["<C-p>"] = cmp.config.disable,
         ["<Tab>"] = cmp.config.disable,
         ["<S-Tab>"] = cmp.config.disable,
         ['<CR>'] = cmp.mapping.confirm({
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,53 +1,97 @@
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v3.x",
+  "neovim/nvim-lspconfig",
   dependencies = {
     -- LSP Support
-    { "neovim/nvim-lspconfig" },
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim" },
-    -- { "jay-babu/mason-null-ls.nvim" },
-
+    { "hrsh7th/cmp-nvim-lsp" },
     -- Autocompletion
     { "hrsh7th/nvim-cmp" },
   },
   config = function()
-    vim.opt.completeopt = { "menu", "menuone", "noselect" }
-    vim.opt.shortmess:append("c")
-    local lsp_zero = require("lsp-zero").preset({})
-    lsp_zero.set_sign_icons({
-      error = " ", warn = " ", hint = " ", info = " "
+    local lspconfig = require('lspconfig')
+    local lsp_defaults = lspconfig.util.default_config
+
+    lsp_defaults.capabilities = vim.tbl_deep_extend(
+      'force',
+      lsp_defaults.capabilities,
+      require('cmp_nvim_lsp').default_capabilities()
+    )
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      desc = 'LSP actions',
+      callback = function(event)
+        local opts = { buffer = event.buf }
+        local keymap = vim.keymap
+        opts.desc = "Show LSP references"
+        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+        opts.desc = "Go to declaration"
+        keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+        opts.desc = "Show LSP definitions"
+        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+        opts.desc = "Show LSP implementations"
+        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+        opts.desc = "Show LSP type definitions"
+        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+        opts.desc = "Code Actions"
+        keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+        opts.desc = "Smart Rename"
+        keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts) -- smart rename
+
+        opts.desc = "Show buffer diagnostics"
+        keymap.set("n", "<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+        opts.desc = "Show line diagnostics"
+        keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+        opts.desc = "Go to previous diagnostic"
+        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+        opts.desc = "Go to next diagnostic"
+        keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+        opts.desc = "Show documentation for what is under cursor"
+        keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+        opts.desc = "Restart LSP"
+        keymap.set("n", "<leader>lc", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+      end
     })
 
-    local lua_opts = lsp_zero.nvim_lua_ls()
-    require('lspconfig').lua_ls.setup(lua_opts)
+    local default_setup = function(server)
+      lspconfig[server].setup({})
+    end
 
-
-    lsp_zero.on_attach(function(_, bufnr)
-      -- see :help lsp-zero-keybindings
-      -- to learn the available actions
-      local opts = { buffer = bufnr }
-      lsp_zero.default_keymaps({ preserve_mappings = false })
-      vim.keymap.set({ 'n', 'x' }, '<leader>f', "<cmd>GuardFmt<cr>", opts)
-      vim.keymap.set({ 'n', 'x' }, '<leader>la', function()
-        vim.lsp.buf.code_action({ async = false, timeout_ms = 10000 })
-      end, opts)
-      vim.keymap.set({ 'n' }, '<leader>lr', function()
-        vim.lsp.buf.rename()
-      end, opts)
-    end)
-
-    require("mason").setup({})
-    require("mason-lspconfig").setup({
-      ensure_installed = {},
-      handlers = {
-        lsp_zero.default_setup,
-        lua_ls = function()
-          local lua_lsp_opts = lsp_zero.nvim_lua_ls()
-          require("lspconfig").lua_ls.setup(lua_lsp_opts)
-        end,
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      ensure_installed = { "tsserver", "eslint", "lua_ls", "ansiblels", "astro", "bashls", "clangd", "cssls", "dockerls",
+        "emmet_ls", "eslint", "html", "jsonls", "tailwindcss", "vimls", "yamlls" },
+      handlers = { default_setup,
+            -- configure lua server (with special settings)
+    lspconfig["lua_ls"].setup({
+      settings = { -- custom settings for lua
+        Lua = {
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            -- make language server aware of runtime files
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+          },
+        },
       },
     })
-
+      },
+    })
   end,
 }
