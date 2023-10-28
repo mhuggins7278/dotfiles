b15933665be3fa8e32e38c16f5599ae41818789a
Mark Huggins <mhuggins@glgroup.com>
2023-10-27 23:28:12 -0400

    style(nvim): clean up color scheme config including transparent windows

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,97 +1,102 @@
 require("rose-pine").setup({
-  disable_background = true,
+	disable_background = true,
 })
 require("tokyonight").setup({
-  transparent = true,
-  styles = {
-    floats = "transparent",
-  },
+	transparent = true,
+	styles = {
+		floats = "transparent",
+	},
 })
 require("catppuccin").setup({
-  flavour = "mocha",
-  term_colors = false,
-  transparent_background = false,
-  integrations = {
-    alpha = true,
-    cmp = true,
-    dap = {
-      enabled = true,
-      enable_ui = true,
-    },
-    gitsigns = true,
-    harpoon = true,
-    lsp_trouble = true,
-    markdown = true,
-    mason = true,
-    native_lsp = {
-      enabled = true,
-      virtual_text = {
-        errors = { "italic" },
-        hints = { "italic" },
-        warnings = { "italic" },
-        information = { "italic" },
-      },
-      underlines = {
-        errors = { "underline" },
-        hints = { "underline" },
-        warnings = { "underline" },
-        information = { "underline" },
-      },
-      inlay_hints = {
-        background = true,
-      },
-    },
-    noice = true,
-    notify = true,
-    nvimtree = true,
-    octo = true,
-    telescope = {
-      enabled = true,
-    },
-    treesitter = true,
-    treesitter_context = true,
-    ts_rainbow2 = true,
-    which_key = true,
-  },
-  styles = {                 -- Handles the styles of general hi groups (see `:h highlight-args`):
-    comments = { "italic" }, -- Change the style of comments
-    conditionals = { "italic" },
-  },
-  compile_path = vim.fn.stdpath "cache" .. "/catppuccin"
+	flavour = "mocha",
+	transparent_background = true,
+	term_colors = true,
+	integrations = {
+		alpha = true,
+		cmp = true,
+		dap = {
+			enabled = true,
+			enable_ui = true,
+		},
+		gitsigns = true,
+		harpoon = true,
+		lsp_trouble = true,
+		markdown = true,
+		mason = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+			},
+			underlines = {
+				errors = { "undercurl" },
+				hints = { "undercurl" },
+				warnings = { "undercurl" },
+				information = { "undercurl" },
+			},
+			inlay_hints = {
+				background = true,
+			},
+		},
+		noice = true,
+		notify = true,
+		nvimtree = true,
+		octo = true,
+		telescope = true,
+		treesitter = true,
+		treesitter_context = true,
+		ts_rainbow2 = true,
+		which_key = true,
+	},
+	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = { "italic" }, -- Change the style of comments
+		conditionals = { "italic" },
+	},
+	compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
 })
 
 require("kanagawa").setup({
-  transparent = true,
+	transparent = true,
 })
 
 require("github-theme").setup({
-  options = {
-    transparent = true,
-  },
-  modules = { -- List of various plugins and additional options
-    octo = true,
-    which_key = true,
-    lualine = true,
-    alpha = true,
-    treesitter = true,
-    telescope = true,
-    harpoon = true,
-    cmp = true,
-    gitsigns = true,
-    mason = true,
-    noice = true,
-    dap = {
-      enabled = true,
-      enable_ui = true,
-    },
-  },
+	options = {
+		compile_path = vim.fn.stdpath("cache") .. "/github-theme",
+		compile_file_suffix = "_compiled", -- Compiled file suffix
+		hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
+		hide_nc_statusline = true, -- Override the underline style for non-active statuslines
+		transparent = false, -- Disable setting background
+		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+		dim_inactive = false, -- Non focused panes set to alternative background
+		module_default = true,
+	},
+	modules = { -- List of various plugins and additional options
+		octo = true,
+		which_key = true,
+		lualine = true,
+		alpha = true,
+		treesitter = true,
+		telescope = { enabled = true },
+		harpoon = true,
+		cmp = true,
+		gitsigns = true,
+		mason = true,
+		noice = true,
+		dap = {
+			enabled = true,
+			enable_ui = true,
+		},
+	},
 })
 
 function ColorMyPencils(color)
-  color = color or "catppuccin"
-  vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	color = color or "catppuccin"
+	vim.cmd.colorscheme(color)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
