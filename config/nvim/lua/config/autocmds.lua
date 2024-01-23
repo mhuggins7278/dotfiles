1e515a0003e797c6bac3a10133dc1d97067baec6
Mark Huggins <mhuggins@glgroup.com>
2024-01-23 09:32:14 -0500

    feat(filetypes): autocmds for snowflake and mustache filetypes
    sets the filetype to be sql

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,26 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+--
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup("snowflake"),
+  callback = function()
+    if vim.o.buftype == "snowflake" then
+      vim.cmd("set filetype=sql")
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup("mustache"),
+  callback = function()
+    if vim.o.buftype == "mustache" then
+      vim.cmd("set filetype=sql")
+    end
+  end,
+})
