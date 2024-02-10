e4393023205f70d3289701291b3b3f5b8c0a9ff2
Mark Huggins <mhuggins@glgroup.com>
2024-02-10 10:58:47 -0500

    style(better-background): last version was to opaque

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -25,11 +25,11 @@ end
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.font_size = 18.0
 config.font = wezterm.font("Monaspace Radon Var", { weight = "Regular" })
-config.window_background_opacity = 0.2
+config.window_background_opacity = 0.7
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
-config.macos_window_background_blur = 30
+config.macos_window_background_blur = 60
 config.term = "wezterm"
 -- and finally, return the configuration to wezterm
 return config
