e0219c285d16b663e93cfc94383ae1a2411f5387
Mark Huggins <mhuggins@glgroup.com>
2025-03-13 16:34:01 -0400

    feat(plugins): remove avante and copilot plugins, add key mappings to code-companion
    - Removed `avante.nvim` plugin configuration.
    - Removed `copilot.lua` plugin configuration.
    - Added key mappings to `code-companion.lua` for toggling chat and committing staged changes.

--- config/nvim/lua/custom/plugins/code-companion.lua
+++ config/nvim/lua/custom/plugins/code-companion.lua
@@ -5,4 +5,9 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
   },
+  keys = {
+    { '<leader>ct', '<cmd>CodeCompanionChat Toggle<cr>', desc = '[T]oggle Chat' },
+    { '<leader>cc', '<cmd>CodeCompanion /commit <cr>', desc = '[C]ommit Staged' },
+  },
+  opts = {},
 }
